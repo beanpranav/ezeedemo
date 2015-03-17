200615170aefef7fe179732522c54c2627c23531
Pranav <bean.pranav@gmail.com>
2015-03-17 20:38:02 +0530

    video content validation and views

--- app/models/video_content.rb
+++ app/models/video_content.rb
@@ -1,8 +1,7 @@
 class VideoContent < ActiveRecord::Base
 
 	has_attached_file :content
-	validates_attachment :content, presence: true, content_type: { content_type: "image/png" } 
-	# "video/mp4" }
+	validates_attachment :content, presence: true, content_type: { content_type: "video/mp4" }
 
 	validates_presence_of :content_type, :name, :video_duration, :production_date, :producer_name
 	
