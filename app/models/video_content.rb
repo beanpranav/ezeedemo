1e28cfe72ab6e600301a6cde6f2441dbd63dbe18
Pranav <bean.pranav@gmail.com>
2015-03-17 20:11:47 +0530

    s3 bucket test

--- app/models/video_content.rb
+++ app/models/video_content.rb
@@ -1,7 +1,8 @@
 class VideoContent < ActiveRecord::Base
 
 	has_attached_file :content
-	validates_attachment :content, presence: true, content_type: { content_type: "video/mp4" }
+	validates_attachment :content, presence: true
+	# , content_type: { content_type: "video/mp4" }
 
 	validates_presence_of :content_type, :name, :video_duration, :production_date, :producer_name
 	
